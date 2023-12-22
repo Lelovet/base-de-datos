2d8185810b1b9af8e518dfb801127d3d73e5eb3a
felipe <111469218+Lelovet@users.noreply.github.com>
2023-12-22 19:59:31 -0300

    Add files via upload

---
+++ trabajo.sql
@@ -0,0 +1,10 @@
+SELECT * FROM trabajo.oradores;
+
+INSERT INTO `trabajo`.`oradores` (`id_orador`,`nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES ('2','pablo', 'fernandez', 'pablito@gmail.com', 'capitales', '2025/12/22');
+INSERT INTO `trabajo`.`oradores` (`id_orador`,`nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES ('3','agustin', 'fredes', 'agusfre@gmail.com', 'programas', '2025/12/22');
+INSERT INTO `trabajo`.`oradores` (`id_orador`,`nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES ('4','elias', 'perez', 'elli@gmail.com', 'empresas', '2025/12/22');
+INSERT INTO `trabajo`.`oradores` (`id_orador`,`nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES  ('5','juan', 'gonzales', 'jm@gmail.com', 'tipos de energia', '2025/12/22');
+INSERT INTO `trabajo`.`oradores` (`id_orador`,`nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES ('6','jose', 'stut', 'josito@gmail.com', 'programacion', '2025/12/22');
+INSERT INTO `trabajo`.`oradores` (`id_orador`,`nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES ('7','joaquin', 'lettieri', 'jk@gmail.com', 'edificios', '2025/12/22');
+INSERT INTO `trabajo`.`oradores` (`id_orador`,`nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES ('8','lautaro', 'lopez', 'lauty@gmail.com', 'canciones', '2025/12/22');
+INSERT INTO `trabajo`.`oradores` (`id_orador`,`nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES ('9','lucas', 'garcia', 'luquitas@gmail.com', 'electronica', '2025/12/22');
